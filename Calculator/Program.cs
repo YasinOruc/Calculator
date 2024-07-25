9c5ec459eec8b34d9bd500040da4738166d529b6
YasinOruc <orucyasin14@gmail.com>
2024-07-25 10:37:02 +0200

    Implement robust input validation for decimal numbers using double.TryParse

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,17 +1,30 @@
 ﻿// Declare variables and then initialize to zero.
 double num1 = 0; double num2 = 0;
+string input;
 
 // Display title as the C# console calculator app.
 Console.WriteLine("Console Calculator in C#\r");
 Console.WriteLine("------------------------\n");
 
+
 // Ask the user to type the first number.
 Console.WriteLine("Type a number, and then press Enter");
-num1 = Convert.ToDouble(Console.ReadLine());
+input = Console.ReadLine();
+
+while (!double.TryParse(input, out num1))
+{
+    Console.WriteLine("Invalid input, please type a numeric value.");
+    input = Console.ReadLine();
+}
 
 // Ask the user to type the second number.
 Console.WriteLine("Type another number, and then press Enter");
-num2 = Convert.ToDouble(Console.ReadLine());
+input = Console.ReadLine(); 
+
+while (!double.TryParse(input, out num2))
+{
+    Console.WriteLine("Invalid input, please type a numeric value.");
+}    
 
 // Ask the user to choose an option.
 Console.WriteLine("Choose an option from the following list:");
